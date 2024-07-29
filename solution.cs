e77448de1bc88c6731d3f48320e2af16a11903ec
Kooreis <kooreis.official@gmail.com>
2024-07-29 03:10:58 -0500

    Added a function to calculate the greatest common divisor (gcd) of two numbers using recursion.

--- solution.cs
+++ solution.cs
@@ -1,4 +1,3 @@
-```javascript
 function gcd(a, b) {
     if (!b) {
         return a;
@@ -6,20 +5,3 @@ function gcd(a, b) {
 
     return gcd(b, a % b);
 }
-
-function lcm(a, b) {
-    return (a * b) / gcd(a, b);
-}
-
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-readline.question('Enter two integers (separated by space): ', (input) => {
-    const numbers = input.split(' ').map(Number);
-    console.log(`GCD of ${numbers[0]} and ${numbers[1]} is: ${gcd(numbers[0], numbers[1])}`);
-    console.log(`LCM of ${numbers[0]} and ${numbers[1]} is: ${lcm(numbers[0], numbers[1])}`);
-    readline.close();
-});
-```
